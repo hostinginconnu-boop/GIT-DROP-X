fd5a54e36abad8fa7e588e991fa05b06ac897e8f
hostinginconnu-boop <hostinginconnu@gmail.com>
2026-06-04 23:12:12 -0300

    INCONNU BOY TECH 🍂

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+inconnu const app = express();
+inconnu const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', engine: 'GitDrop X ⚡', author: 'Inconnu Boy 🖤' });
+});
+
+app.listen(PORT, () => {
+  console.log(`GitDrop X ⚡ running on port ${PORT}`);
+});
+
+module.exports = app;
